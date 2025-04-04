d86714f4aa5a9bd5b49d249c28f61bae054b7aed
Francisco1333 <francis14322@gmail.com>
2025-04-03 21:26:27 -0600

    Tarea grupal progra 1

---
+++ codigo_10_Francisco.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "   45" << endl;
+    cout << " x 32" << endl;
+    cout << "------" << endl;
+    cout << "   90" << endl;
+    cout << " 1350" << endl;
+    cout << "------" << endl;
+    cout << " 1440" << endl;
+    
+    return 0;
+}
+
